b3509e928f4066af542ed29986ad716492e215e6
Ryan Geddes <rgeddes09@gmail.com>
2020-04-09 21:50:22 -0700

    class02 commit

--- js/test.js
+++ js/test.js
@@ -1,3 +1,51 @@
+'use strict';
+
+console.log('123')
+
+//document.body.textContent = 'Hello';
+
+// // anchor data to the parent element
+var pEl = document.getElementById('movies');
+
+// // create new element
+// var liEl = document.createElement('li');
+
+// // give new element some content
+// liEl.textContent = 'This is my list of items.';
+
+// // append the child element to the parent element
+// pEl.appendChild(liEl);
+
+var colors = ['red', 'black', 'yellow', 'blue', 'purple'];
+
+for(var i = 0; i < colors.length; i++){
+    // create element
+    var liEl = document.createElement('li');
+    // give it content
+    liEl.textContent = `${colors[i]} is the Way`;
+    // append the child to the parent
+    pEl.appendChild(liEl);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //daily cookie calculator:
 // calculate random cookies 14 times 
 // store to array
